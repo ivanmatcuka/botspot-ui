209ed0d91d8f94e548df42ea9f91322f11d86d78
Ivan Matcuka <ivanmatcuka@protonmail.com>
2025-04-23 12:17:01 +0200

    wip: more types

--- src/types/contact-form-7.ts
+++ src/types/contact-form-7.ts
@@ -27,3 +27,22 @@ export type CF7Form = {
   };
   title: string;
 };
+
+export type Field = {
+  attr: string;
+  basetype: string;
+  content: string;
+  labels: string[];
+  name: string;
+  options: string[];
+  pipes: Record<string, unknown>;
+  raw_name: string;
+  raw_values: string[];
+  type: string;
+  values: string[];
+};
+
+export type ApiResponse = {
+  fields: Field[];
+  status: number;
+};
--- types/types/contact-form-7.d.ts
+++ types/types/contact-form-7.d.ts
@@ -26,3 +26,20 @@ export type CF7Form = {
     };
     title: string;
 };
+export type Field = {
+    attr: string;
+    basetype: string;
+    content: string;
+    labels: string[];
+    name: string;
+    options: string[];
+    pipes: Record<string, unknown>;
+    raw_name: string;
+    raw_values: string[];
+    type: string;
+    values: string[];
+};
+export type ApiResponse = {
+    fields: Field[];
+    status: number;
+};
